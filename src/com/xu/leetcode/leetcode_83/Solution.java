9a1c793f1afb7489ae0a8935287b50eded7fcc51
文享旭 <846212939@qq.com>
2022-03-02 10:13:54 +0800

    83. 删除排序链表中的重复元素

---
+++ src/com/xu/leetcode/leetcode_83/Solution.java
@@ -0,0 +1,40 @@
+package com.xu.leetcode.leetcode_83;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null) {
+            if(slow.val != fast.val) {
+                slow = slow.next;
+                slow.val = fast.val;
+            }
+            fast = fast.next;
+        }
+        slow.next = null;
+        return head;
+    }
+
+     public static class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+
+}
